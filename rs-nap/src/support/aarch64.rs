1ccb36c31e3c50e62680950eb44359c02b7f8b18
Jonathan DeMarks <jmdemarks@spscommerce.com>
2023-09-25 13:37:36 -0500

    fix for register clobbering

--- rs-nap/src/support/aarch64.rs
+++ rs-nap/src/support/aarch64.rs
@@ -22,13 +22,17 @@ pub unsafe fn sys_exit(exit_code:usize) -> ! {
 }
 
 pub unsafe fn sys_write(buffer: *const u8, count: usize) {
-    asm!("mov  x0, #1",
-         "mov  x1, {buffer}",
-         "mov  x2, {count}",
-         "mov  x8, #64",
-         "svc #0",
-         buffer = in(reg) buffer,
-         count = in(reg) count
+    asm!(
+        "mov  x0, #1",
+        "mov  x1, {buffer}",
+        "mov  x2, {count}",
+        "mov  x8, #64",
+        "svc #0",
+        buffer = inout(reg) buffer => _,
+        count = inout(reg) count => _,
+        out("x0") _,
+        out("x8") _,
+        options(nostack)
     )
 }
 
