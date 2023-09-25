4f518b4a2488a864aa64209e9a5957f04bff38a6
Jonathan DeMarks <jmdemarks@spscommerce.com>
2023-09-25 13:56:37 -0500

    Clean up sys_write implementation

--- rs-nap/src/support/aarch64.rs
+++ rs-nap/src/support/aarch64.rs
@@ -22,16 +22,11 @@ pub unsafe fn sys_exit(exit_code:usize) -> ! {
 }
 
 pub unsafe fn sys_write(buffer: *const u8, count: usize) {
-    asm!(
-        "mov  x0, #1",
-        "mov  x1, {buffer}",
-        "mov  x2, {count}",
-        "mov  x8, #64",
-        "svc #0",
-        buffer = inout(reg) buffer => _,
-        count = inout(reg) count => _,
-        out("x0") _,
-        out("x8") _,
+    asm!("svc #0",
+        inout("x0") 1 => _,
+        inout("x1") buffer => _,
+        inout("x2") count => _,
+        inout("x8") 64 => _,
         options(nostack)
     )
 }
